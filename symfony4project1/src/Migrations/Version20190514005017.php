d67fb5319a5e5235c2c8526f16a606e37a76fd6c
DominikStyp <dominik.stypula1@gmail.com>
2019-05-14 02:59:09 +0200

    Migrations re-generation

--- symfony4project1/src/Migrations/Version20190514005017.php
+++ symfony4project1/src/Migrations/Version20190514005017.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190513235728 extends AbstractMigration
+final class Version20190514005017 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,9 +23,9 @@ final class Version20190513235728 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, status SMALLINT NOT NULL, INDEX IDX_5A8A6C8DA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_5A8A6C8DA76ED395 ON post (user_id)');
     }
 
     public function down(Schema $schema) : void
@@ -35,7 +35,7 @@ final class Version20190513235728 extends AbstractMigration
 
         $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8DA76ED395');
         $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE post');
         $this->addSql('DROP TABLE user');
-        $this->addSql('DROP INDEX IDX_5A8A6C8DA76ED395 ON post');
     }
 }
