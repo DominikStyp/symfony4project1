673017077e361c846e13d71e7a32330388db823f
DominikStyp <dominik.stypula1@gmail.com>
2019-05-21 01:29:49 +0200

    use TimestampableEntity; to make Entity timestampable (Laravel like). Plus generated migrations for the date/time fields.

--- symfony4project1/src/Entity/Category.php
+++ symfony4project1/src/Entity/Category.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
@@ -28,6 +29,8 @@ class Category
      * @ORM\ManyToMany(targetEntity="App\Entity\Post", mappedBy="categories")
      */
     private $posts;
+
+    use TimestampableEntity;
 
     public function __construct()
     {
--- symfony4project1/src/Entity/User.php
+++ symfony4project1/src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -37,6 +38,8 @@ class User
      * @ORM\OneToMany(targetEntity="App\Entity\Post", mappedBy="user", orphanRemoval=true)
      */
     private $posts;
+
+    use TimestampableEntity;
 
     public function __construct()
     {
