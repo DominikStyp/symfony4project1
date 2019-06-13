1052a219e40bb223a49a88c8afd082a5ca18d35d
DominikStyp <dominik.stypula1@gmail.com>
2019-06-13 03:05:57 +0200

    LOADING FIXTURES BEFORE EVERY TEST CLASS USING doctrine:fixtures:load

--- symfony4project1/tests/CommonTestCase.php
+++ symfony4project1/tests/CommonTestCase.php
@@ -9,6 +9,10 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class CommonTestCase extends KernelTestCase {
     /**
@@ -16,13 +20,79 @@ class CommonTestCase extends KernelTestCase {
      */
     protected $entityManager;
 
+
+    /**
+     * @param Application $application
+     * @return int
+     * @throws \Exception
+     */
+    private static function dropSchema(Application $application){
+
+        $command = $application->find('doctrine:schema:drop');
+        $arguments = [
+            'command' => 'doctrine:schema:drop',
+            '--force' => true
+
+        ];
+        $input = new ArrayInput($arguments);
+        $output = new ConsoleOutput();
+        $returnCode = $command->run($input,$output);
+        return $returnCode;
+    }
+
+
+    /**
+     * @param Application $application
+     * @return int
+     * @throws \Exception
+     */
+    private static function createSchema(Application $application){
+
+        $command = $application->find('doctrine:schema:create');
+        $arguments = [
+            'command' => 'doctrine:schema:create'
+        ];
+        $input = new ArrayInput($arguments);
+        $output = new ConsoleOutput();
+        $returnCode = $command->run($input,$output);
+        return $returnCode;
+    }
+
+
+    /**
+     * @param Application $application
+     * @return int
+     * @throws \Exception
+     */
+    private static function loadDatabaseFixtures(Application $application){
+        $command = $application->find('doctrine:fixtures:load');
+        $arguments = [
+            'command' => 'doctrine:fixtures:load',
+        ];
+        $input = new ArrayInput($arguments);
+        $input->setInteractive(false);
+        $output = new ConsoleOutput();
+        $returnCode = $command->run($input, $output);
+        return $returnCode;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function setUpBeforeClass() {
+        parent::setUpBeforeClass();
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+        self::loadDatabaseFixtures($application);
+    }
+
     /**
      * {@inheritDoc}
+     * @throws \Exception
      */
     protected function setUp()
     {
         $kernel = self::bootKernel();
-
         $this->entityManager = $kernel->getContainer()
             ->get('doctrine')
             ->getManager();
