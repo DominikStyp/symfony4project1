fa869b02192baab1f7eb451afa6bad6989d74e00
aniat <ania.topolska@wp.pl>
2019-06-15 01:48:36 +0200

    Doctrine Event Listeners (Lifecycle Events) testing

---
+++ symfony4project1/src/EventListeners/MyTestEventListener.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aniat
+ * Date: 15.06.2019
+ * Time: 01:14
+ */
+
+namespace App\EventListeners;
+
+
+class MyTestEventListener
+{
+
+}
---
+++ symfony4project1/tests/Repository/DoctrineEventsListenersTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aniat
+ * Date: 15.06.2019
+ * Time: 00:57
+ */
+
+namespace App\Tests\Repository;
+
+
+class DoctrineEventsListenersTest
+{
+
+}
