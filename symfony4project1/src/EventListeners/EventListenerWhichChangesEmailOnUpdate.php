00f785bf648a2d194f6f7bcdb756730f72f2b9fb
aniat <ania.topolska@wp.pl>
2019-06-15 03:17:13 +0200

    change listener name

---
+++ symfony4project1/src/EventListeners/EventListenerWhichChangesEmailOnUpdate.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aniat
+ * Date: 15.06.2019
+ * Time: 01:14
+ */
+
+namespace App\EventListeners;
+
+
+use App\Entity\User;
+use \Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+
+class MyTestEventListener
+{
+    const TEST_EMAIL = "changed@gmail.com";
+
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+        $entityManager = $args->getObjectManager();
+        // perhaps you only want to act on some "Product" entity
+        if ($entity instanceof User) {
+            $entity->setEmail(self::TEST_EMAIL);
+        }
+    }
+}
