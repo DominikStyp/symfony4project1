25060b78860ff0e3d8dd5c2ced2db341508e437c
DominikStyp <dominik.stypula1@gmail.com>
2019-07-01 23:41:44 +0200

    REFACTORING STUPID SYMFONY CACHE DESIGN

--- symfony4project1/tests/SymfonyCacheTest.php
+++ symfony4project1/tests/SymfonyCacheTest.php
@@ -13,36 +13,43 @@ class SymfonyCacheTest extends CommonTestCase {
 
     const CACHE_KEY = 'my_cache_key';
     private $cacheMisses = 0;
+    /** @var FilesystemAdapter */
+    private $cache;
     /**
      * @throws \Psr\Cache\InvalidArgumentException
      */
     public function testSymfonyCacheFileAdapter() {
+
+        $this->cache = new FilesystemAdapter();
+        // attempt nr 1
+        $this->assertEquals("time_consuming_value_1", $this->getCachedValue());
+        $this->assertEquals(1, $this->cacheMisses);
+        // attempt nr 2 - cache should not expire yet
+        $this->assertEquals("time_consuming_value_1", $this->getCachedValue());
+        // attempt nr 3 - cache should not expire yet
+        $this->assertEquals("time_consuming_value_1", $this->getCachedValue());
+        $this->assertEquals(1, $this->cacheMisses);
+        sleep(3);
+        // cache SHOULD expire NOW and should be RE-COMPUTED
+        $this->assertEquals("time_consuming_value_2", $this->getCachedValue());
+        $this->assertEquals(2, $this->cacheMisses);
+
+    }
+
+    /**
+     * @return mixed
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    private function getCachedValue(){
         $cacheMisses = & $this->cacheMisses;
-        $cache = new FilesystemAdapter();
-        // The callable will only be executed on a cache miss.
-        $value = $cache->get(self::CACHE_KEY, function (ItemInterface $item) use (& $cacheMisses) {
+        $value = $this->cache->get(self::CACHE_KEY, function (ItemInterface $item) use (& $cacheMisses) {
             $cacheMisses++;
             $item->expiresAfter(2);
             $computedValue = "time_consuming_value_{$cacheMisses}";
-            echo PHP_EOL, "Retrieving {$computedValue} from cache", PHP_EOL;
+            echo PHP_EOL, ">>>>>>>> Retrieving {$computedValue} from cache", PHP_EOL;
             return $computedValue;
         });
-
-        // attempt nr 1
-        $this->assertEquals(1, $this->cacheMisses);
-        $this->assertEquals("time_consuming_value_1", $cache->getItem(self::CACHE_KEY)->get());
-        // attempt nr 2 - cache should not expire yet
-        $this->assertEquals(1, $this->cacheMisses);
-        $this->assertEquals("time_consuming_value_1", $cache->getItem(self::CACHE_KEY)->get());
-        // attempt nr 3 - cache should not expire yet
-        $this->assertEquals(1, $this->cacheMisses);
-        $this->assertEquals("time_consuming_value_1", $cache->getItem(self::CACHE_KEY)->get());
-
-        sleep(3);
-        // cache SHOULD expire NOW and should be RE-COMPUTED
-        $this->assertEquals("time_consuming_value_2", $cache->getItem(self::CACHE_KEY)->get());
-        $this->assertEquals(2, $this->cacheMisses);
-
-        $cache->delete(self::CACHE_KEY);
+        var_dump($value);
+        return $value;
     }
 }
